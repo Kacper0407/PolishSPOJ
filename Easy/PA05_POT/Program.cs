476f1bdef5b73e0799fc3010a7a93bd51a6fdfa1
Kacper Dabrowski <dabrowskikacper001@gmail.com>
2023-10-06 12:55:21 +0200

    PA05_POT - początek

---
+++ Easy/PA05_POT/Program.cs
@@ -0,0 +1,20 @@
+﻿class Program
+{
+    public static void Main(string[] args)
+    {
+        int n = Convert.ToInt32(Console.ReadLine());
+        int[] wynik = new int [n]; 
+        for(int i=0; i<n; i++)
+        {
+            string[] input = Console.ReadLine().Split(' ');
+            int a = Convert.ToInt32(input[0]);
+            int b = Convert.ToInt32(input[1]);
+
+            //wynik[i] = a%10;
+        }
+        for(int i=0; i<n; i++)
+        {
+            Console.WriteLine(wynik[i]);
+        }
+    }
+}
