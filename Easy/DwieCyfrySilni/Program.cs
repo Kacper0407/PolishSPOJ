e1268c2be2fedef22abf59c968ef01a179788dfe
Kacper Dabrowski <dabrowskikacper001@gmail.com>
2023-10-06 11:25:16 +0200

    DwieCyfrySilni - mniejsza pamięć
    Początkowy koncpet zuzywający mniejszą ilość pamięci

--- Easy/DwieCyfrySilni/Program.cs
+++ Easy/DwieCyfrySilni/Program.cs
@@ -4,22 +4,31 @@
     {
         byte n = Convert.ToByte(Console.ReadLine());
         int num = 0;
+        int[] silnie = new int [n];
         for(int i=0; i<n; i++)
         {
             num = Convert.ToInt32(Console.ReadLine());
-            int silnia = 1;
             if(num<10)
             {
-                for(int j=2; j<=num; j++)
-                {
-                    silnia*=j;    
-                }
-                Console.WriteLine($"{(silnia/10)%10} {silnia%10}");
+                silnie[i] = Silnia(num);       
             }
             else
             {
-                Console.WriteLine($"{0} {0}");
+                silnie[i] = 0;
             }
         }
+        for(int i=0; i<n; i++)
+        {
+            Console.WriteLine($"{(silnie[i]/10)%10} {silnie[i]%10}");
+        }
+    }
+    static int Silnia(int num)
+    {
+        int silnia = 1;
+        for(int j=2; j<=num; j++)
+        {
+            silnia*=j;    
+        }
+        return silnia;
     }
 }
