0b2d3004b3d9d77f3a0994a172a68cee4611e191
Kacper Dabrowski <dabrowskikacper001@gmail.com>
2023-10-06 08:58:05 +0200

    DwieCyfrySilni - good
    Zaakceptowana odpowiedź

--- Easy/DwieCyfrySilni/Program.cs
+++ Easy/DwieCyfrySilni/Program.cs
@@ -2,27 +2,24 @@
 {
     static void Main(string[] args)
     {
-        int n = Convert.ToInt32(Console.ReadLine());
-        int[] silnie = new int [n];
+        byte n = Convert.ToByte(Console.ReadLine());
+        int num = 0;
         for(int i=0; i<n; i++)
         {
-            int num = Convert.ToInt32(Console.ReadLine());
-            silnie[i] = Silnia(num);
-        }
-        for(int i=0; i<n; i++)
-        {
-            Console.WriteLine($"{silnie[i]/10} {silnie[i]%10}");
+            num = Convert.ToInt32(Console.ReadLine());
+            int silnia = 1;
+            if(num<10)
+            {
+                for(int j=2; j<=num; j++)
+                {
+                    silnia*=j;    
+                }
+                Console.WriteLine($"{(silnia/10)%10} {silnia%10}");
+            }
+            else
+            {
+                Console.WriteLine($"{0} {0}");
+            }
         }
     }
-    public static int Silnia(int num)
-    {
-        int result = 1;
-        while (num != 1)
-        {
-            result *= num;
-            num --;
-        }
-        return result;
-    }
-    
 }
--- Easy/DwieCyfrySilni/Silnia.c
+++ Easy/DwieCyfrySilni/Silnia.c
@@ -1,32 +1,40 @@
 #include <stdlib.h>
 #include <stdio.h>
-int silnia(int);
 void main()
 {
     int n = 0;
-    int* silnie;
     scanf("%d", &n);
     int num = 0;
-
-    silnie = (int*)calloc(n, sizeof(int));
     for(int i=0; i<n; i++)
     {
         scanf("%d", &num);
-        silnie[i] = silnia(num);
+        switch(num){
+                case 1:
+                case 2:
+                case 3:
+                case 4:
+                   for(int j=num-1; j>0; j--){
+                        num = num*j;
+                    }
+                   break;
+                case 5:
+                case 6:
+                    num = 20;
+                    break;
+                case 7:
+                    num = 40;
+                    break;
+                case 8:
+                    num = 20;
+                    break;
+                case 9:
+                    num = 80;
+                    break;
+                default:
+                    num = 0;
+                    break;
+            }
+        printf("%d %d\n", num/10, num%10);
     }
     
-    for(int i=0; i<n; i++)
-    {
-        printf("%d %d\n", silnie[i]/10, silnie[i]%10);
-    }
-}
-int silnia(int num)
-{
-    int result = 1;
-    while (num != 1)
-    {
-        result *= num;
-        num --;
-    }
-    return result;
 }
